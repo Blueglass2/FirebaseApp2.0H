ac97d5f44ef0a3bba3abfa5d6e43239eb4662d60
Blueglass2 <78095712+Blueglass2@users.noreply.github.com>
2021-03-01 09:59:46 -0600

    New Stuff!
    Added logo, data of games now show. Working on Update Function rn

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,5 +1,70 @@
 
+/*This file will contain all the JS that will communicate with 
+the firebase firestore database and also firebase auth that is needed to authenticate
+authorized users.  If you don't use auth, then some of this code is not needed.
+*/
 
+auth.onAuthStateChanged(user => {
+    //  if the user was logged out, then user parameter will be null
+    //  if the user is logged in, then user parameter will equal a reference to their account (auth token)
+
+        if (user) {
+        // get data if a valid user is logged in
+            //setupUI(user);      // display correct links in navbar based on login status
+
+            /*
+            In this section of code, we are getting the logs collection and then calling the onSnapshot method.
+            This will grab a current snapshot of the state of the database.  This is done every time there is a
+            change to the database, but only if the user is logged in.
+
+            Because we are using the onSnapshot method, this will execute every time there is a change to the database.  
+            This will be what causes our web app to update in real time.  So IF the user is logged in, and a change is made,
+            then the user will see it on the web app.
+
+            The parameter snapshot refers to a current static copy of the database at this exact moment in time.
+            docChanges is a method that is called of the snapshot and we are using a for each loop to cycle through
+            all the changes for each entry of the log.  If the change was 'added', we need to call renderLog to add a 
+            new Student <li> entry to our display.  If the change was 'removed', we need to query that specific <li> tag
+            from the DOM and remove it so it is removed from the display.  If you were also allowing the display to update
+            in realtime, then you would have another option here for 'modified'.
+
+            The else statement for the top function is what to do if the user is NOT logged in.  In this case
+            we want to make sure that the logs do NOT display and that the UI displays the appropriate links to Log in or 
+            Sign up
+            */
+            db.collection('player').onSnapshot(snapshot => {
+                
+                let changes = snapshot.docChanges();
+                // based on change made, either add to display or remove from display
+                changes.forEach(change => {
+                   if (change.type == 'added') {
+                       renderLog(change.doc)
+                       
+                    }
+                    else if (change.type == 'removed') {
+                        // finding the li in the DOM of the document that was just removed
+                        const idToRemove = 'id' + change.doc.id;
+
+                        let li = logList.querySelector('[data-id=' + idToRemove + ']');
+                        // remove this li from the ul
+                        logList.removeChild(li);
+                    }
+                    // if an entry was updated, the change type is 'modified'
+                    // if you wanted to listen for changes in the db, you'd need this added it if statement
+                })
+            });
+        }
+        else {
+            setupUI();  // don't send anything so it will evaluate to false in the method
+            setupLogs([]);
+        }
+    });
+
+/*
+    This function creates a new account using a new email and password and saves it on firebase.
+    The account can then be used to log into this program once created.
+
+*/
 const signupForm = document.querySelector('#signUp-form');
 
 function signUp() {
@@ -18,18 +83,22 @@ function signUp() {
 }
 
 
-
+/*
+    This function logs the account(if registered) into the program and changes to the login screen to the data screen
+*/
 function signIn() {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
+    var email = document.getElementById("idEmail").value;
+    var password = document.getElementById("idPassword").value;
     
     console.log("Clicked sign in with " + email + " " + password);
     
     //Successful -> .then code
     //Unsuccessful -> catch code (catch error message)
     auth.signInWithEmailAndPassword(email, password).then(() => {
-        console.log("Singed in " + email);
-
+        console.log("Signed in " + email);
+        
+        document.getElementById("idEmail").value = "";
+        document.getElementById("idPassword").value = "";
         document.getElementById("idLoginScreen").style.display = "none";
         document.getElementById("idDataScreen").style.display = "block";
 
@@ -38,7 +107,8 @@ function signIn() {
     
 }
 
-
+/*This function signs out of the account being used and takes the user back to the login screen
+*/
 function signOut() {
     auth.signOut();
     console.log("Signed Out");
@@ -47,15 +117,19 @@ function signOut() {
     document.getElementById("idDataScreen").style.display = "none";
 }
 
+/*
+    This form is used to add the video game name, console name, and hours played onto the firebase storage
+
+*/
 const addForm = document.querySelector('#data-form');
 
 addForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(addForm['idName'].value)
 
-    db.collection('players').add({
-        name: addForm['idName'].value,
-        score: addForm['idScore'].value
+    db.collection('player').add({
+        videoGameName: addForm['idGameName'].value,
+        consoleName: addForm['idConsoleName'].value,
+        hoursPlayed: addForm['idGameHours'].value
         
     }).then(()=> {
         // get a reference to the open modal
--- scripts/realtime.js
+++ scripts/realtime.js
@@ -3,19 +3,199 @@
 // in order to use it, we need to add the firestore SDK link in
 // our HTML file and create a firestore const like we did for auth
 
-
-
-var nameV, scoreV    // values of the input boxes in the HTML
+var gameNameV, consoleV, hoursPlayedV;    // values of the input boxes in the HTML
  
 function ready() {
-   nameV = document.getElementById('idName').value;
-   scoreV = document.getElementById('idScore').value;
+  gameNameV = document.getElementById('idGameNameUp').value;
+  consoleV = document.getElementById('idConsoleNameUp').value;
+  hoursPlayedV = document.getElementById('idGameHoursUp').value;
+   
 }
 
 
+const logList = document.querySelector('.player');
+const loggedOutLinks = document.querySelectorAll('.logged-out'); // need All bc there are > 1
+const loggedInLinks = document.querySelectorAll('.logged-in');
+
+/*
+    SET UP UI 
+
+    In this function we toggle the links based on whether the user is signed in or not
+    This function is called from auth.js when the auth status changes. If someone logs in
+    or logs out, then their auth status has changed, therefore this method is called and the 
+    user auth token is passed to it.  If there is a user, then the first statement will show
+    the links that have the class of logged-in and will hide the links with a class of logged-out.
+
+    If the user isn't logged in, then nothing is passed to this function, therefore the else will
+    execute, and in this case the links are toggled the other way.  
+*/
+
+const setupUI = (user) => {
+  if (user) {
+    // toggle UI elements with a forEach loop that will change the display to show or hide them
+    loggedInLinks.forEach(item => item.style.display = 'block');  // show
+    loggedOutLinks.forEach(item => item.style.display = 'none');  // hide
+
+  }
+  else {
+    loggedInLinks.forEach(item => item.style.display = 'none');  // hide
+    loggedOutLinks.forEach(item => item.style.display = 'block');  // sho
+  }
+}
 
 
+const form = document.querySelector('#create-form');
+
+/*
+    GENERATE ONE ROW OF Player DATA
+
+    This function generates each <li> row for the data in the database. Each time
+    a player is added or deleted, the change in onSnapshot method call in onAuthStateChanged in
+    auth.js.  When that add happens, this function is called and it is passed a parameter, 
+    doc, which is the document that needs to be added.  This parameter contains the data for the 
+    document in the database, which we can access via the .data() method and then the attribute 
+    name after that such as: doc.data().name;
+
+    So... what the function is doing is first creating the HTML tags that will be used to build up the
+    row of data for this log to add.  After that, we add a attribute for data-id which is the unique ID
+    value for the document that way later we have a way to connect it back to the database.  
+
+    Next we set the text we want to display for this HTML element.
+    After that, I set the class property for the HTML elements for their CSS styles
+    From there, we begin building up the HTML that will be added to the DOM using the 
+    append child tag.  We build up the <li> and then add the <li> to the <ul>
+
+    Lastly, I added a listener for the delete "button" for each player.  This works by getting the
+    gets unique id that we set as an attribute in its HTML tag.  Once we have that id, we have what we
+    need to call the delete function from firestore on the 'logs' collection.
+
+
+*/
+function renderLog(doc) {
+
+    let li = document.createElement('li');
+    let gameName = document.createElement('span');
+    let gameConsole = document.createElement('span');
+    let gameHours = document.createElement('span');
+    let deleteX = document.createElement('span');
+    let newLine = document.createElement('br');
+    let newLine2 = document.createElement('br');
+    // we need to attach the UID to the li tag so that way if we need to 
+    // access it later, we know which element it was
+
+    // I am adding the string id in front of the UID because if the UID starts with a 
+    // number instead of a letter, then it isn't a valid id and the query won't work when
+    // trying to update or delete the document in the database
+     
+    const idVal = 'id' + doc.id;
+    li.setAttribute('data-id', idVal);
+
+    // Set the text for the elements to display
+    gameName.textContent = 'Video game: ' + doc.data().videoGameName;
+    gameConsole.textContent = 'Console: ' + doc.data().consoleName;
+    gameHours.textContent = 'Hours Played: ' + doc.data().hoursPlayed;
+    deleteX.textContent = 'Delete';
+
+    // set the class for the styles to apply to the HTML tag
+    deleteX.classList = 'deleteX';
+    gameName.classList = 'logLI';
+    gameConsole.classList = 'logLI';
+    gameHours.classList = 'logLI';
+
+    li.appendChild(gameName);
+    li.appendChild(newLine);
+    li.appendChild(gameConsole);
+    li.appendChild(newLine2);
+    li.appendChild(gameHours);
+    li.appendChild(deleteX);
+    logList.appendChild(li);
+
+    // Add listener for the delete option
+    deleteX.addEventListener('click', (e) => {
+      e.stopPropagation();
+      var id = e.target.parentElement.getAttribute('data-id');  
+      id = id.substring(2);
+      db.collection('player').doc(id).delete();
+    })
+    
+  };
+
+// SEARCH/SHOW DATA FUNCTIONALITY - ONCLICK
+/*
+    This code has two different search capabilities - first by
+    idNum and then by name. The idea is that the user would type
+    in a name (with the rest of the fields blank) and
+    then press the show data button to have the rest of the info
+    shown. They could also do the same thing with the
+    id number.
+
+    Using the where() function allows me to do a query and
+    limit the values that are returned from the collection
+
+    bad assumptions - name and id are unique. She didn't really
+    design this project for multiple documents with same name / id
+
+
+    When we call get(), we are getting a snapshot of the database,
+    which is essentially an instance of the database (or collection)
+    at that moment in time
+
+    After getting this snapshot, you can iterate through the snapshot
+    and see all the documents that are in it.
+*/
+
+function showData(){
+   ready();
+   console.log(gameNameV + " " + consoleV + " " + hoursPlayedV);
+   db.collection('player').where("idGameNameUp", '==', gameNameV).get().then((snapshot) => {
+       snapshot.forEach((doc) => {
+           console.log(doc.id, " => ", doc.data());
+           console.log(doc.data().idGameNameUp);
+           document.getElementById('idGameNameUp').value = doc.data().videoGameName;
+           document.getElementById('idConsoleNameUp').value = doc.data().consoleName;
+           document.getElementById('idGameHoursUp').value = doc.data().hoursPlayed;
+           
+       });
+   }).catch((e) => {
+       console.log("Error getting game by name: ", e.message);
+   });
+}; 
+
+// UPDATE FUNCTIONALITY - ONCLICK
+
+function update(){
+   ready();
+   console.log(gameNameV + " " + consoleV + " " + hoursPlayedV);
+   db.collection('player').where("idGameNameUp", '==', gameNameV).get().then((snapshot) => {
+       snapshot.forEach((doc) => {
+           console.log(doc.id, " => ", doc.data());
+           // gets UID so we can locate the document to update it in collection
+           const updateID = doc.id;
+          console.log("Pain");
+           // if any of these fields are empty, we then keep old value
+           if (gameNameV == undefined) {
+               gameNameV = doc.data().videoGameName;
+           }
+           if (consoleV == undefined) {
+               consoleV = doc.data().consoleName;
+           }
+           if (idGameHours == undefined) {
+               idGameHours = doc.data().hoursPlayed;
+           }
  
+           // sets new value to this existing student record.  Update will only
+           // change the values we ask it to change. 
+           db.collection('player').doc(updateID).update({
+               videoGameName: gameNameV,
+               consoleName: consoleV,
+               hoursPlayed: hoursPlayedV,
+           }).then(() => {
+               console.log("Updated, new data:");
+           }).catch(e => console.log(e.message));
+       });
+   });
+   
+};
 /*
     SET UP MATERIALIZE COMPONENTS
     **  You only need this if you are using the Materialize styling and the code you
